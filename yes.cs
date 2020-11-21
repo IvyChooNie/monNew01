23afe287e3497d3d2ef0397c7bb68046a1fb1727
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2020-11-21 22:18:56 +0800

    Add files via upload

---
+++ yes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class yes : MonoBehaviour
+{
+    public void id()
+    {
+        switch (this.gameObject.name)
+        {
+            case "resume":
+                SceneManager.LoadScene("Menu");
+                break;
+
+                        
+               
+        }
+    }
+}
